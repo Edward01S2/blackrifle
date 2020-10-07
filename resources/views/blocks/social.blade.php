6a9c6bd44c10f39e598d60f7d8496944ca9302cd
edward01s2 <edward.01s2@gmail.com>
2020-10-06 23:02:19 -0500

    Change social again

--- resources/views/blocks/social.blade.php
+++ resources/views/blocks/social.blade.php
@@ -8,15 +8,15 @@
     <div class="grid gap-y-4 md:grid-cols-2 md:gap-x-6 md:gap-y-6 lg:gap-y-8 lg:gap-x-8 lg:max-w-4xl lg:mx-auto xl:max-w-5xl 2xl:max-w-6xl">
       @foreach($gallery as $item)
         <div class="relative w-full pb-full">
-          <img class="absolute inset-0 object-cover object-center w-full h-full img-share" src="{!! $item['url'] !!}" alt="" data-title="America Needs Coffee" data-url="{{ home_url('/') }}">
-          {{-- <div class="absolute bottom-0 right-0 flex items-center justify-end w-full mb-4 mr-4 space-x-4 a2a_kit lg:mb-8 lg:mr-8 lg:space-x-6" data-a2a-url="{!! $site !!}" data-a2a-title="Black Rifle Coffee" data-a2a-media="{!! $site . $item['url'] !!}">
+          <img class="absolute inset-0 object-cover object-center w-full h-full img-share" src="{!! $item['url'] !!}" alt="">
+          <div class="absolute bottom-0 right-0 flex items-center justify-end w-full mb-4 mr-4 space-x-4 a2a_kit lg:mb-8 lg:mr-8 lg:space-x-6">
             <a class="image-share a2a_button_facebook">
               @svg('images.facebook', 'w-6 h-6 fill-current text-white hover:text-c-red-100 lg:w-8 lg:h-8')
             </a>
             <a class="image-share a2a_button_twitter">
               @svg('images.twitter', 'w-6 h-6 fill-current text-white hover:text-c-red-100 lg:w-8 lg:h-8')
             </a>
-          </div> --}}
+          </div>
         </div>
       @endforeach
     </div>
