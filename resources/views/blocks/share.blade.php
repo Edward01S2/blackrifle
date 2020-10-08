5cf984430311469c8206130713e16666da72d3c9
edward01s2 <edward.01s2@gmail.com>
2020-10-07 23:56:27 -0500

    Img share

--- resources/views/blocks/share.blade.php
+++ resources/views/blocks/share.blade.php
@@ -8,8 +8,8 @@
     <div class="grid gap-y-4 md:grid-cols-2 md:gap-x-6 md:gap-y-6 lg:gap-y-8 lg:gap-x-8 lg:max-w-4xl lg:mx-auto xl:max-w-5xl 2xl:max-w-6xl">
       @foreach($share as $item)
         <div class="relative w-full pb-full">
-          <img class="absolute inset-0 object-cover object-center w-full h-full img-share" src="{!! $item['img'] !!}" alt="">
-          <div class="absolute bottom-0 right-0 flex items-center justify-end w-full mb-4 mr-4 space-x-4 a2a_kit lg:mb-8 lg:mr-8 lg:space-x-6" data-a2a-url="{!! $item['link'] !!}">
+          <img class="absolute inset-0 object-cover object-center w-full h-full img-share" src="{!! $item['img'] !!}" alt="" data-title="Black Rifle Coffee" data-summary="This is a summary">
+          <div class="absolute bottom-0 right-0 flex items-center justify-end w-full mb-4 mr-4 space-x-4 a2a_kit lg:mb-8 lg:mr-8 lg:space-x-6">
             <a class="image-share a2a_button_facebook">
               @svg('images.facebook', 'w-6 h-6 fill-current text-white hover:text-c-red-100 lg:w-8 lg:h-8')
             </a>
